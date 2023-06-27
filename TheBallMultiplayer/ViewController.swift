64cd46b439ade22ee1599c56ee7540f08614ec10
Yuchen Huang <charinhuang@gmail.com>
2023-06-27 00:33:04 -0700

    Initial Commit

---
+++ TheBallMultiplayer/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  TheBallMultiplayer
+//
+//  Created by Yuchen Huang on 6/27/23.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
